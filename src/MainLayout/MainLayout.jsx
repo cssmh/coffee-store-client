5bab119d8888fc398d66cc3dea071e114f5faf61
momen210 <touristmomen@gmail.com>
2024-02-10 01:17:40 +0600

    added dynamic title

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,9 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Component/Navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import Footer from "../Component/Footer/Footer";
+import { useEffect } from "react";
 
 const MainLayout = () => {
+
+  // Dynamic title
+  const loc = useLocation()
+  useEffect(()=>{
+    const path = loc.pathname.toUpperCase();
+    if(path === "/"){
+      document.title = "Coffee Master-Home"
+    }
+    else if(loc.state){
+      document.title = `${loc.state}`
+    }
+    else{
+      document.title = `Coffee${path.replace("/", "-")}`
+    }
+  },[loc.pathname, loc.state])
+  // Dynamic title End
+
   return (
     <div>
       <Navbar></Navbar>
